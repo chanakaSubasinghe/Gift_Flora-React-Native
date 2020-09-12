be8be6da2fedc77ce552a02b7b8cc5ed8e14759c
chanaka subasinghe <chanaka.s@icloud.com>
2020-09-12 11:18:48 +0530

    add layout structure with colors

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import BottomNav from './src/components/BottomNav';
 import HomeScreen from './src/screens/HomeScreen';
-import MenuScreen from './src/screens/MenuScreen';
 
 // importing components
 export default function App() {
   return (
-    <HomeScreen />
+    <View style={styles.container}>
+      <SafeAreaView style={styles.safeAreaView} />
+      <HomeScreen />
+      <BottomNav />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    backgroundColor: '#5f4b8bff'
+  },
+  container: {
+    flex: 1
+  }
+});
+
+
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,103 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BottomNav from '../components/BottomNav';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import uuid from 'uuid';
 
 export default function HomeScreen() {
+
+    const [categories, setCategories] = useState([
+        {
+            id: uuid(),
+            categoryName: 'Birthday',
+        },
+        {
+            id: uuid(),
+            categoryName: 'Anniversary',
+        },
+        {
+            id: uuid(),
+            categoryName: 'Birthday',
+        }
+    ]);
+
     return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-            <BottomNav />
+        // <View>
+        //     <Text style={styles.text}>Home Screen</Text>
+        //     <FlatList
+        //         horizontal
+        //         data={categories}
+        //         renderItem={({ item }) => (<Text>{item.categoryName}</Text>)}
+        //     />
+        // </View>
+
+        <View style={{ marginHorizontal: 20 }}>
+
+            <View style={{
+                height: '7%',
+                backgroundColor: 'gray',
+                marginVertical: 15
+            }} />
+
+
+            <View style={{
+                height: '20%',
+                backgroundColor: 'lightblue',
+                marginVertical: 15
+            }} />
+
+            <Text style={{ fontSize: 30, marginVertical: 15 }}>Categories</Text>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 20 }}>
+                <View style={{
+                    height: 60,
+                    width: 60,
+                    borderRadius: 100,
+                    backgroundColor: 'lightpink'
+                }} />
+
+                <View style={{
+                    height: 60,
+                    width: 60,
+                    borderRadius: 100,
+                    backgroundColor: 'lightpink'
+                }} />
+
+                <View style={{
+                    height: 60,
+                    width: 60,
+                    borderRadius: 100,
+                    backgroundColor: 'lightpink'
+                }} />
+
+                <View style={{
+                    height: 60,
+                    width: 60,
+                    borderRadius: 100,
+                    backgroundColor: 'lightpink'
+                }} />
+            </View>
+
+            <Text style={{ fontSize: 30, marginVertical: 15 }}>Most Popular</Text>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 20 }}>
+
+                <View style={{
+                    height: 180,
+                    width: 170,
+                    borderRadius: 10,
+                    backgroundColor: 'orange'
+                }} />
+
+                <View style={{
+                    height: 180,
+                    width: 170,
+                    borderRadius: 10,
+                    backgroundColor: 'orange'
+                }} />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+
 });
