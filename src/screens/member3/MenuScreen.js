169d5da493947eabb5a50dda5875e1b2d15fec09
chanaka subasinghe <chanaka.s@icloud.com>
2020-09-13 22:38:12 +0530

    add member directories

--- src/screens/member3/MenuScreen.js
+++ src/screens/member3/MenuScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import BottomNav from '../components/BottomNav';
+import BottomNav from '../../components/BottomNav';
 
 
 export default function MenuScreen() {
