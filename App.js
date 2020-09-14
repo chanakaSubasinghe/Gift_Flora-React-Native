62c0fc0e7ca28da6171cbf0e364e903c69935dfa
chanaka subasinghe <chanaka.s@icloud.com>
2020-09-14 07:45:22 +0530

    change screens between members

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import BottomNav from './src/components/BottomNav';
-import HomeScreen from './src/screens/HomeScreen';
+import HomeScreen from './src/screens/member3/HomeScreen';
 
 // importing components
 export default function App() {
